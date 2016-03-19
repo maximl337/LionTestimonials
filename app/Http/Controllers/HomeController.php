4f30099cbb117819197eb1a5a26a7d0478e48f9f
Angad Dubey <angad_dubey@Hotmail.com>
2016-03-19 18:02:59 -0400

    fix auth redirect issue

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,11 +19,7 @@ class HomeController extends Controller
     }
 
     public function welcome()
-    {
-        if(Auth::check()) {
-            return redirect('home');
-        }
-        
+    {   
         return view('welcome');
     }
 
