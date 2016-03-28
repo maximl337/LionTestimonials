4b9b8e816d5188e20c6dc6eb15e2bd93b9dc0dfc
Angad Dubey <angad_dubey@Hotmail.com>
2016-03-28 16:00:05 -0400

    approvable

--- resources/views/testimonials/show.blade.php
+++ resources/views/testimonials/show.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
+@section('head')
+    <style type="text/css">
+        video {
+            max-width: 100%;
+            max-height: 400px;
+        }
+    </style>
+@endsection
 @section('content')
 <div class="container">
     <div class="row">
@@ -10,7 +18,10 @@
                 </div>
 
                 <div class="panel-body">
-
+                    
+                    @if(!is_null($testimonial->approved_at))
+                        <div class="pull-right"><span class="label label-success"></span></div>
+                    @endif
                     <div class="text-center">
                         <h1 style="font-size: 120px;">{{ $testimonial->rating }}</h1>
                         <p><em>out of 5 (rating)</em></p>
@@ -32,6 +43,14 @@
                     <p>{{ $testimonial->body }}</p>
 
                     <p>From: <strong>{{ $testimonial->contact()->first()->first_name }}</strong> <span class="pull-right">{{ $testimonial->created_at->diffForHumans() }}</span></p>
+
+                    <p>
+                        
+                        @if(is_null($testimonial->approved_at))
+                            <a href="#" data-id="{{ $testimonial->id }}" class="approve btn btn-small btn-primary">Approve</a>
+                        @endif
+                        
+                    </p>
                     
                 </div> <!-- .panel-body -->
 
@@ -61,4 +80,48 @@
 </script>
 @endif
 
+<script type="text/javascript">
+$(function() {
+
+    $(document).on("click", ".approve", function(e) {
+        e.preventDefault();
+
+        $this = $(this);
+
+        $this.addClass('disabled');
+
+        $this.html('<i class="fa fa-cog fa-spin"></i>');
+
+        var sendData = {
+            _token: "{{ csrf_token() }}",
+            id: $this.data('id'),
+        };
+
+        $.ajax({
+            type : "POST",
+            url : "testimonials/approve",
+            data : sendData,
+            //contentType: "application/json; charset=UTF-8",
+            success: function (response) {  
+                $this.removeClass('btn-primary').addClass('btn-success').html('Approved');
+            },
+            statusCode: {
+                403: function() {
+                    swal("Uh oh!", "Forbidden request", "error");
+                },
+                404: function() {
+                    swal("Uh oh!", "Could not find the resource", "error");
+                },
+                500: function() {
+                    swal("Uh oh!", "Internal server error", "error");
+                }
+            },
+            error: function (e) {
+                swal("Uh oh!", e, "error");
+            } 
+        });
+
+    });
+});
+</script>
 @endsection
