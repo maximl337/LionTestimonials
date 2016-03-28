b668a3d5709204c4d7f300189c06bf7231363048
Angad Dubey <angad_dubey@Hotmail.com>
2016-03-28 19:27:10 -0400

    remove accept

--- resources/views/testimonials/_partials/create-from-phone.blade.php
+++ resources/views/testimonials/_partials/create-from-phone.blade.php
@@ -56,7 +56,7 @@
 
                 <div id="mobile-video" class="form-group{{ $errors->has('video') ? ' has-error' : '' }}">
                     <label for="video">Video</label>
-                    <input class="form-control" type="file" name="video" accept="*" capture="camcorder">
+                    <input class="form-control" type="file" name="video" capture="camcorder">
                     @if ($errors->has('video'))
                         <span class="help-block">
                             <strong>{{ $errors->first('video') }}</strong>
