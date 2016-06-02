97b2fe291c12fd15cf169962c0dad4d04299a0eb
simplesphere <anoop.jeewoolall@gmail.com>
2016-06-02 19:34:16 -0400

    changed layout and corrected URLs

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+
             <div class="panel panel-default">
 
                 <div class="panel-heading">Contacts <a href="{{ url('contacts/create') }}" class="pull-right"><i class="fa fa-pencil"></i> Create</a></div>
@@ -113,9 +111,6 @@
                     
                 </div> <!-- .panel-body -->
             </div>
-        </div>
-    </div>
-</div>
 @endsection
 
 @section('footer')
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,24 +2,23 @@
 
 @section('content')
 
-
   <ul class="quick-options row">
   	<li class="col-md-3">
   		<a href="{{ url('contacts') }}"><i class="font-icon ion-email-unread"></i>Contacts</a>
   	</li>
   	<li class="col-md-3">
-  		<a href="{{ url('contacts') }}"><i class="font-icon ion-ios-bookmarks-outline"></i>Testimonials</a>
+  		<a href="{{ url('testimonials') }}"><i class="font-icon ion-ios-bookmarks-outline"></i>Testimonials</a>
   	</li>
   	<li class="col-md-3">
-  		<a href="{{ url('contacts') }}"><i class="font-icon ion-ios-camera-outline"></i>Videos</a>
+  		<a href="{{ url('videos') }}"><i class="font-icon ion-ios-camera-outline"></i>Videos</a>
   	</li>
   	<li class="col-md-3">
-  		<a href="{{ url('contacts') }}"><i class="font-icon ion-ios-cog-outline"></i>Profile</a>
+  		<a href="{{ url('profile') }}"><i class="font-icon ion-ios-cog-outline"></i>Profile</a>
   	</li>
   </ul>
 
   <hr/>
-  
+
   <div class="panel panel-default">
     <div class="panel-heading">Dashboard</div>
     <div class="panel-body">
