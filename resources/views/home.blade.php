fa5b589c23a86e8734623f36cd01dd0955256956
simplesphere <anoop.jeewoolall@gmail.com>
2016-06-02 19:28:40 -0400

    changed colors and home layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
+
+
+  <ul class="quick-options row">
+  	<li class="col-md-3">
+  		<a href="{{ url('contacts') }}"><i class="font-icon ion-email-unread"></i>Contacts</a>
+  	</li>
+  	<li class="col-md-3">
+  		<a href="{{ url('contacts') }}"><i class="font-icon ion-ios-bookmarks-outline"></i>Testimonials</a>
+  	</li>
+  	<li class="col-md-3">
+  		<a href="{{ url('contacts') }}"><i class="font-icon ion-ios-camera-outline"></i>Videos</a>
+  	</li>
+  	<li class="col-md-3">
+  		<a href="{{ url('contacts') }}"><i class="font-icon ion-ios-cog-outline"></i>Profile</a>
+  	</li>
+  </ul>
+
+  <hr/>
+  
   <div class="panel panel-default">
     <div class="panel-heading">Dashboard</div>
     <div class="panel-body">
