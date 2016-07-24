9b59763edafba3d2953bae52a9192525fa97811e
Angad Dubey <angad_dubey@Hotmail.com>
2016-07-24 11:43:48 -0400

    update contract

--- app/Contracts/VideoToGif.php
+++ app/Contracts/VideoToGif.php
@@ -4,7 +4,7 @@ namespace App\Contracts;
 
 interface VideoToGif {
 
-	public function convert($video_url);
+	public function convert($video_url, $video_id);
 
 	public function save(array $input);
 }
